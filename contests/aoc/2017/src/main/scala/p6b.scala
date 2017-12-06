555339a1311db7aeb2c1ea97c85f45a3116975c9
Axiometry <marco.vanhilst@gmail.com>
2017-12-06 04:38:37 -0500

    AOC2017 P6

---
+++ contests/aoc/2017/src/main/scala/p6b.scala
@@ -0,0 +1,19 @@
+object p6b extends App {
+  val initial = readLine.split("\\W+").toIndexedSeq.map(_.toInt)
+  val n = initial.size
+  
+  var state = Map(initial -> 0)
+  var last = initial
+  var num = 1
+  while({
+    val (v, i) = last.zipWithIndex.maxBy { case (v, i) => (v, -i) }
+    val next = for(j <- last.indices) yield (v/n) + (if((j-i-1+n)%n < v%n) 1 else 0) + (if(i == j) 0 else last(j))
+    last = next
+    if(!state.contains(next)) {
+      state = state.updated(next, num)
+      num += 1
+      true
+    } else false
+  }) ()
+  println(num - state(last))
+}
