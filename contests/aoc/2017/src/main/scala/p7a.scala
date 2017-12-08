eb9202bc59b4e45385ed637aa950ce0488806004
Axiometry <marco.vanhilst@gmail.com>
2017-12-07 19:29:52 -0500

    AOC2017 P7

---
+++ contests/aoc/2017/src/main/scala/p7a.scala
@@ -0,0 +1,17 @@
+import scala.util.matching.Regex
+
+object p7a extends App {
+  val NoChildren = Set.empty[String]
+  implicit class RegexContext(sc: StringContext) {
+    def r = new Regex(sc.parts.mkString, sc.parts.tail.map(_ => "x"): _*)
+  }
+  
+  val lines = io.Source.fromInputStream(System.in).getLines.toSeq
+  val children = lines.map {
+    case r"([a-z]+)$name [^ ]+ -> (.*)$ch" => (name, ch split ", " toSet)
+    case r"([a-z]+)$name .*" => (name, NoChildren)
+  }.toMap
+  
+  val haveParent = children.values.reduce(_|_)
+  println(children.keys.find(!haveParent.contains(_)).get)
+}
---
+++ contests/aoc/2017/src/main/scala/p7b.scala
@@ -0,0 +1,49 @@
+import scala.annotation.tailrec
+import scala.collection.IterableView
+import scala.collection.immutable.Queue
+import scala.util.matching.Regex
+
+object p7b extends App {
+  val NoChildren = Set.empty[String]
+  implicit class RegexContext(sc: StringContext) {
+    def r = new Regex(sc.parts.mkString, sc.parts.tail.map(_ => "x"): _*)
+  }
+  object SetViewExt {
+    def unapplySeq[T](s: IterableView[T, Set[T]]): Option[Seq[T]] = Some(s.toSeq)
+  }
+  
+  val lines = io.Source.fromInputStream(System.in).getLines.toSeq
+  val children = lines.map {
+    case r"([a-z]+)$name [^ ]+ -> (.*)$ch" => (name, ch split ", " toSet)
+    case r"([a-z]+)$name .*" => (name, NoChildren)
+  }.toMap
+  val weights = lines.map {
+    case r"""([a-z]+)$name \(([0-9]+)$weight\).*""" => (name, weight.toInt)
+  }.toMap
+  
+  @tailrec def toposort(weightTotals: Map[String, Int], q: Queue[String]): Option[Int] = q match {
+    case dude +: qs =>
+      val ch = children(dude).view
+      val byWeight = ch.groupBy(weightTotals)
+      
+      if(byWeight.size <= 1) { // balanced
+        val newW = weightTotals.updated(dude, ch.map(weightTotals).sum + weights(dude))
+        val newQ = qs ++ children.collect {
+          case (n2, ch2) if !newW.contains(n2) && ch2.contains(dude) && ch2.forall(newW.contains) => n2
+        }
+        
+        toposort(newW, newQ)
+      } else { // one weight is off
+        val (badWeight, SetViewExt(badDude)) = byWeight.minBy(_._2.size)
+        val (goodWeight, _)             = byWeight.maxBy(_._2.size)
+        
+        Some(weights(badDude) + (goodWeight - badWeight))
+      }
+    case _ => None
+  }
+  
+  val leaves = children.collect {
+    case (dude, NoChildren) => dude
+  }
+  println(toposort(Map.empty, leaves.to).get)
+}
